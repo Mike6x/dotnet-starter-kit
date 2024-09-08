d40fc3ad933fa471c2236f6e5ed8280ba50ea259
mike.p6x <mike.p6x@gmail.com>
2024-09-08 22:07:38 +0700

    add dimension page

--- src/apps/blazor/client/Pages/Setting/Dimensions.razor.cs
+++ src/apps/blazor/client/Pages/Setting/Dimensions.razor.cs
@@ -11,9 +11,9 @@ public partial class Dimensions : ComponentBase
     [Inject]
     protected IApiClient ApiClient { get; set; } = default!;
 
-    protected EntityServerTableContext<DimensionDto, Guid, DimensionViewModel> Context { get; set; } = default!;
+    protected EntityServerTableContext<GetDimensionResponse, Guid, DimensionViewModel> Context { get; set; } = default!;
 
-    private EntityTable<DimensionDto, Guid, DimensionViewModel> _table = default!;
+    private EntityTable<GetDimensionResponse, Guid, DimensionViewModel> _table = default!;
 
     protected override void OnInitialized() =>
         Context = new(
@@ -28,35 +28,22 @@ public partial class Dimensions : ComponentBase
                 new(item => item.Name, "Name", "Name"),
                 new(item => item.Value, "Value", "Value" ),
                 new(item => item.Type, "Type", "Type" ),
-
+                // new(item => item.FatherName, "Father item", "FatherName"),
                 new(item => item.FullName, "Full Name", "FullName"),
                 new(item => item.NativeName, "Native Name", "NativeName"),
 
                 // new(item => item.FullNativeName, "Full Native", "FullNativeName"),
                 // new(item => item.Description, "Description", "Description"),
-                new(item => item.FatherName, "Father", "Father.Name"),
-                // new(item => item.FatherId, "Father", "FatherId"),
-                
                 new(item => item.IsActive,  "Active", Type: typeof(bool)),
             },
             enableAdvancedSearch: false,
             idFunc: item => item.Id,
-            exportFunc: async filter =>
-            {
-                var dataFilter = filter.Adapt<ExportDimensionsRequest>();
-                dataFilter.Type = SearchTypeString;
-                
-                return await ApiClient.ExportDimensionsEndpointAsync("1", dataFilter);
-            },
-            importFunc: async (fileUploadModel, isUpdate) => await ApiClient.ImportDimensionsEndpointAsync("1", isUpdate, fileUploadModel),
             searchFunc: async filter =>
             {
-                var dataFilter = filter.Adapt<SearchDimensionsRequest>();
-                dataFilter.Type = SearchTypeString;
-                
-                var result = await ApiClient.SearchDimensionsEndpointAsync("1", dataFilter);
-                
-                return result.Adapt<PaginationResponse<DimensionDto>>();
+                var searchFilter = filter.Adapt<PaginationFilter>();
+
+                var result = await ApiClient.GetDimensionListEndpointAsync("1", searchFilter);
+                return result.Adapt<PaginationResponse<GetDimensionResponse>>();
             },
             createFunc: async item =>
             {
@@ -76,20 +63,11 @@ public partial class Dimensions : ComponentBase
     {
         _ = _table?.ReloadDataAsync();
     }
-    
-    private Guid _searchFatherId;
-    private Guid SearchFatherId
-    {
-        get => _searchFatherId;
-        set
-        {
-            _searchFatherId = value;
-        }
-    }
-
     #endregion
 }
 
 public class DimensionViewModel : UpdateDimensionCommand
 {
 }
+
+
