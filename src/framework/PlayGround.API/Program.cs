122f2d45afc3e8fd58784784ffbf7ff5e3697285
Mukesh Murugan <iammukeshm@gmail.com>
2025-11-01 22:30:37 +0530

    add cors

--- src/framework/PlayGround.API/Program.cs
+++ src/framework/PlayGround.API/Program.cs
@@ -1,9 +1,13 @@
+using FSH.Framework.Web.Cors;
 using FSH.Framework.Web.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.EnableApiDocs(builder.Configuration);
+builder.Services.EnableCors(builder.Configuration);
 var app = builder.Build();
 app.ExposeApiDocs();
+app.ExposeCors();
+
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "hello world!");
---
+++ src/framework/Web/Cors/CorsOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace FSH.Framework.Web.Cors;
+public sealed class CorsOptions
+{
+    public const string SectionName = "Cors";
+
+    public bool AllowAll { get; init; } = true;
+    public string[] AllowedOrigins { get; init; } = [];
+    public string[] AllowedHeaders { get; init; } = ["*"];
+    public string[] AllowedMethods { get; init; } = ["*"];
+}
