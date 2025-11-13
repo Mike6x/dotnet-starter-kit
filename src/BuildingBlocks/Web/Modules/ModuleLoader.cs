8ffa0f75c6765997b572a1a71270db88ce0eb795
Mukesh Murugan <31455818+iammukeshm@users.noreply.github.com>
2025-11-14 00:25:37 +0530

    Add module metadata and validate platform options

--- src/BuildingBlocks/Web/Modules/ModuleLoader.cs
+++ src/BuildingBlocks/Web/Modules/ModuleLoader.cs
@@ -1,6 +1,9 @@
 ﻿// FSH.Framework.Web/Modules/ModuleLoader.cs
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace FSH.Framework.Web.Modules;
@@ -8,20 +11,41 @@ namespace FSH.Framework.Web.Modules;
 public static class ModuleLoader
 {
     private static readonly List<IModule> _modules = new();
+    private static readonly object _lock = new();
+    private static bool _modulesLoaded;
 
     public static IHostApplicationBuilder AddModules(this IHostApplicationBuilder builder, params Assembly[] assemblies)
     {
-        var source = assemblies is { Length: > 0 } ? assemblies : AppDomain.CurrentDomain.GetAssemblies();
-
-        foreach (var type in source.SelectMany(a => a.DefinedTypes))
+        lock (_lock)
         {
-            if (type.IsAbstract || !typeof(IModule).IsAssignableFrom(type)) continue;
-
-            if (Activator.CreateInstance(type) is IModule module)
+            if (_modulesLoaded)
             {
+                return builder;
+            }
+
+            var source = assemblies is { Length: > 0 }
+                ? assemblies
+                : AppDomain.CurrentDomain.GetAssemblies();
+
+            var moduleRegistrations = source
+                .SelectMany(a => a.GetCustomAttributes<FshModuleAttribute>());
+
+            foreach (var registration in moduleRegistrations
+                .Where(r => typeof(IModule).IsAssignableFrom(r.ModuleType))
+                .DistinctBy(r => r.ModuleType)
+                .OrderBy(r => r.Order)
+                .ThenBy(r => r.ModuleType.Name))
+            {
+                if (Activator.CreateInstance(registration.ModuleType) is not IModule module)
+                {
+                    throw new InvalidOperationException($"Unable to create module {registration.ModuleType.Name}.");
+                }
+
                 module.ConfigureServices(builder);
                 _modules.Add(module);
             }
+
+            _modulesLoaded = true;
         }
 
         return builder;
